3cef80ceef1a79358cf08a42bfad8422c7c1eea0
Andrew Feldman <afeldman@neuralmagic.com>
2024-02-02 03:27:05 -0500

    added 2:4 example (not actually using 2:4 yet\!)

--- examples/offline_inference_semi_structured_sparse.py
+++ examples/offline_inference_semi_structured_sparse.py
@@ -1,12 +1,14 @@
 from vllm import LLM, SamplingParams
 
-model = LLM("nm-testing/zephyr-50sparse-24",
-            sparsity="semi_structured_sparse_w16a16",
-            enforce_eager=True,
-            dtype="float16",
-            tensor_parallel_size=1,
-            max_model_len=1024)
+model = LLM(
+    "nm-testing/zephyr-50sparse-24", 
+    sparsity="sparse_w16a16",   # If left off, model will be loaded as dense
+    enforce_eager=True,         # Does not work with cudagraphs yet
+    dtype="float16",
+    tensor_parallel_size=1,
+    max_model_len=1024
+)
 
 sampling_params = SamplingParams(max_tokens=100, temperature=0)
 outputs = model.generate("Hello my name is", sampling_params=sampling_params)
-print(outputs[0].outputs[0].text)
+outputs[0].outputs[0].text
