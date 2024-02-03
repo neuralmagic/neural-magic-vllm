48ea1fcddb29fb5fda9c122f839678d16a030919
Andrew Feldman <afeldman@neuralmagic.com>
2024-02-03 06:24:22 -0500

    restructuring sparsity

--- vllm/model_executor/layers/sparsity/sparse_w16a16.py
+++ vllm/model_executor/layers/sparsity/sparse_w16a16.py
@@ -7,6 +7,8 @@ from vllm.model_executor.layers.sparsity.base_config import SparsityConfig
 from .sparse_w16a16_linear_method import SparseW16A16LinearMethod
 from magic_wand import (CompressedStorageFormat, SparseBEGemmStorageFormat)
 
+from .sparse_w16a16_linear_method import SparseW16A16LinearMethod
+from magic_wand import CompressedStorageFormat,SparseBitmaskStorageFormat
 
 class SparseW16A16Config(SparsityConfig):
     """Config class for SparseW16A16.
