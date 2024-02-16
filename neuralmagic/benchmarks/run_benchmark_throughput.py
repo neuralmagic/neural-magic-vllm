8961fbcd3bc4896f63c9cfd5d8b7857fa1bd9405
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-02-16 18:10:49 +0000

    wip

--- neuralmagic/benchmarks/run_benchmark_throughput.py
+++ neuralmagic/benchmarks/run_benchmark_throughput.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 from neuralmagic.tools.call_cmd import call_cmd
 from common import download_datasets, script_args_to_cla
@@ -7,10 +7,12 @@ from common import download_datasets, script_args_to_cla
 def get_this_script_dir() -> Path:
     return Path(__file__).parent.resolve()
 
-def run_benchmark_throughput_script(config:NamedTuple, output_directory:Path) -> None:
+def run_benchmark_throughput_script(config:NamedTuple, output_directory:Optional[Path] = None) -> None:
+
+    assert config.script_name == 'benchmark_throughput.py'
 
     # Process config.download_dataset_cmds
-    #download_datasets(config)
+    download_datasets(config)
 
     script_path = get_this_script_dir() / f"scripts/{config.script_name}"
 
@@ -19,8 +21,11 @@ def run_benchmark_throughput_script(config:NamedTuple, output_directory:Path) ->
             bench_cmd = (
                 ["python3", f"{script_path}"]
                 + script_args
-                + ["--save-directory", f"{output_directory}"]
                 + ["--model", f"{model}"]
                 + ["--tokenizer", f"{model}"]
             )
+
+            if output_directory:
+                bench_cmd = bench_cmd + ["--save-directory", f"{output_directory}"]
+
             call_cmd(bench_cmd, stdout=None, stderr=None)
--- neuralmagic/benchmarks/scripts/common.py
+++ neuralmagic/benchmarks/scripts/common.py
@@ -2,6 +2,9 @@
 Common functions used in all benchmarking scripts
 """
 
+from pathlib import Path
+from typing import NamedTuple, Iterable
+
 def get_bench_environment() -> dict:
     """
     Return the current python version, pytorch version and CUDA version as a dict
@@ -14,3 +17,10 @@ def get_bench_environment() -> dict:
         "torch_cuda_version" : f"{torch.version.cuda}",
         "cuda_device(0)": f"{torch.cuda.get_device_properties(0)}"
     }
+
+def benchmark_configs(config_file_path : Path) -> Iterable[NamedTuple]:
+    """
+    Give a path to a config file in `neuralmagic/benchmarks/configs/*` return an Iterable of
+    (sub)configs in the file
+    """
+    pass
