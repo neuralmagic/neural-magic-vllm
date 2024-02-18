615045927809c7565b22b7ea0badbcb84a12ee07
rsnm2 <rshaw@neuralmagic.com>
2024-02-18 21:20:19 +0000

    fixed main so I can pass ruff

--- vllm/model_executor/layers/sparsity/sparse_w16a16.py
+++ vllm/model_executor/layers/sparsity/sparse_w16a16.py
@@ -5,7 +5,8 @@ import torch
 from vllm.model_executor.layers.sparsity.base_config import SparsityConfig
 
 from .sparse_w16a16_linear_method import SparseW16A16LinearMethod
-from magic_wand import CompressedStorageFormat,SparseBitmaskStorageFormat
+from magic_wand import CompressedStorageFormat, SparseBEGemmStorageFormat
+
 
 class SparseW16A16Config(SparsityConfig):
     """Config class for SparseW16A16.
