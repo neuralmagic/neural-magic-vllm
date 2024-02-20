c381ba549c3e78fb4bcb8c857a80854cb4bc08cd
mgoin <michael@neuralmagic.com>
2024-02-20 21:55:30 +0000

    Format

--- vllm/model_executor/layers/sparsity/sparse_w16a16.py
+++ vllm/model_executor/layers/sparsity/sparse_w16a16.py
@@ -6,7 +6,7 @@ from vllm.logger import init_logger
 from vllm.model_executor.layers.sparsity.base_config import SparsityConfig
 
 from .sparse_w16a16_linear_method import SparseW16A16LinearMethod
-from magic_wand import (CompressedStorageFormat, SparseBitmaskStorageFormat, 
+from magic_wand import (CompressedStorageFormat, SparseBitmaskStorageFormat,
                         SparseBEGemmStorageFormat)
 
 logger = init_logger(__name__)
