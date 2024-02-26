44f7b2a7a8731dc4748abc43304b42c557930719
rsnm2 <rshaw@neuralmagic.com>
2024-02-26 18:05:49 +0000

    addressed comments

--- tests/models/test_sparse.py
+++ tests/models/test_sparse.py
@@ -10,8 +10,8 @@ import pytest
 import torch
 from compare_utils import check_logprobs_close
 
-model_format_pairs = [
-    ("nm-testing/Llama-2-7b-pruned50-retrained", "sparse_w16a16"),
+MAX_MODEL_LEN = 1024
+MODEL_FORMAT_PAIRS = [
     ("nm-testing/TinyLlama-1.1B-Chat-v1.0-pruned2.4",
      "semi_structured_sparse_w16a16"),
     ("nm-testing/OpenHermes-2.5-Mistral-7B-pruned50", "sparse_w16a16"),
@@ -19,13 +19,12 @@ model_format_pairs = [
      "semi_structured_sparse_w16a16"),
 ]
 
-
-@pytest.mark.parametrize("model_format_pairs", model_format_pairs)
+@pytest.mark.parametrize("model_format_pairs", MODEL_FORMAT_PAIRS)
 @pytest.mark.parametrize("dtype", ["half"])
 @pytest.mark.parametrize("max_tokens", [32])
 @pytest.mark.parametrize("num_logprobs", [3])
 def test_models(
-    vllm_runner_sparse,
+    vllm_runner_nm,
     example_prompts,
     model_format_pairs,
     dtype: str,
@@ -34,31 +33,27 @@ def test_models(
 ) -> None:
     model_name, sparsity = model_format_pairs
 
-    sparse_model = vllm_runner_sparse(model_name=model_name,
-                                      sparsity=sparsity,
-                                      dtype=dtype,
-                                      max_model_len=1024)
+    sparse_model = vllm_runner_nm(model_name=model_name,
+                                  sparsity=sparsity,
+                                  dtype=dtype,
+                                  max_model_len=MAX_MODEL_LEN)
     sparse_outputs = sparse_model.generate_greedy_logprobs(
         example_prompts, max_tokens, num_logprobs)
 
-    # Note: not sure why, but deleting just the model on Ada Lovelace
-    #   does not free the GPU memory. On Ampere, deleting the just model
-    #   frees the memory.
+    # Note: deleting just the model does not always free the GPU memory, not sure why.
     del sparse_model.model.llm_engine.driver_worker
     del sparse_model
     torch.cuda.empty_cache()
     gc.collect()
 
-    dense_model = vllm_runner_sparse(model_name,
-                                     sparsity=None,
-                                     dtype=dtype,
-                                     max_model_len=1024)
+    dense_model = vllm_runner_nm(model_name=model_name,
+                                 sparsity=None,
+                                 dtype=dtype,
+                                 max_model_len=MAX_MODEL_LEN)
     dense_outputs = dense_model.generate_greedy_logprobs(
         example_prompts, max_tokens, num_logprobs)
 
-    # Note: not sure why, but deleting just the model on Ada Lovelace
-    #   does not free the GPU memory. On Ampere, deleting the just model
-    #   frees the memory.
+    # Note: deleting just the model does not always free the GPU memory, not sure why.
     del dense_model.model.llm_engine.driver_worker
     del dense_model
     torch.cuda.empty_cache()
