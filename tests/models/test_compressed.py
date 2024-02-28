7b885e6575c770e515f1b7db8f9c8d19b190a6c6
rsnm2 <rshaw@neuralmagic.com>
2024-02-28 17:21:02 +0000

    tweaked tolerance

--- tests/models/test_compressed.py
+++ tests/models/test_compressed.py
@@ -23,7 +23,7 @@ MODEL_FORMAT_PAIRS = [
 @pytest.mark.parametrize("model_format_pairs", MODEL_FORMAT_PAIRS)
 @pytest.mark.parametrize("dtype", ["half"])
 @pytest.mark.parametrize("max_tokens", [32])
-@pytest.mark.parametrize("num_logprobs", [3])
+@pytest.mark.parametrize("num_logprobs", [5])
 def test_models(
     vllm_runner_nm,
     example_prompts,
