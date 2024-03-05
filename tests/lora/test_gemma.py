ada2800ec3809411f0d99f8427aefedaf64bdd1f
rsnm2 <rshaw@neuralmagic.com>
2024-03-05 20:39:12 +0000

    gemma passing locally

--- tests/lora/test_gemma.py
+++ tests/lora/test_gemma.py
@@ -1,3 +1,4 @@
+import pytest
 import vllm
 from vllm.lora.request import LoRARequest
 
@@ -26,6 +27,7 @@ def do_sample(llm, lora_path: str, lora_id: int) -> str:
     return generated_texts
 
 
+@pytest.mark.flaky(reruns=2)
 def test_gemma_lora(gemma_lora_files):
     llm = vllm.LLM(MODEL_PATH,
                    max_model_len=1024,
