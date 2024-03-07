f53ad168af4804baa41402aaf32ee37c75c20a14
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-03-07 16:50:30 -0500

    appease ruff

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -375,9 +375,6 @@ class AsyncLLMEngine:
     def get_tokenizer(self):
         return self.engine.tokenizer.tokenizer
 
-    def get_tokenizer(self):
-        return self.engine.tokenizer.tokenizer
-
     def start_background_loop(self) -> None:
         """Start the background loop."""
         if self.errored:
