5bc7a73116a67525b3692a2ecd9a51a5838b4e29
Robert Shaw <rshaw@neuralmagic.com>
2024-03-14 23:24:49 +0000

    formating

--- vllm/model_executor/layers/sparsity/__init__.py
+++ vllm/model_executor/layers/sparsity/__init__.py
@@ -7,9 +7,11 @@ if not is_magic_wand_available:
         "magic_wand is not available and required for sparsity "
         "support. Please install it with `pip install nm-magic-wand`")
 
-from vllm.model_executor.layers.sparsity.base_config import SparsityConfig  # noqa: E402
-from vllm.model_executor.layers.sparsity.sparse_w16a16 import SparseW16A16Config  # noqa: E402
-from vllm.model_executor.layers.sparsity.semi_structured_sparse_w16a16 import SemiStructuredSparseW16A16Config  # noqa: E402
+from vllm.model_executor.layers.sparsity.base_config import SparsityConfig          # noqa: E402
+from vllm.model_executor.layers.sparsity.sparse_w16a16 import SparseW16A16Config    # noqa: E402
+from vllm.model_executor.layers.sparsity.semi_structured_sparse_w16a16 import (
+    SemiStructuredSparseW16A16Config  # noqa: E402
+)
 
 _SPARSITY_CONFIG_REGISTRY = {
     "sparse_w16a16": SparseW16A16Config,
