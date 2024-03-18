5ef5f3c698b25fa165d968efb0e5dd58b161a530
rsnm2 <rshaw@neuralmagic.com>
2024-03-18 17:28:20 +0000

    fixed bad merge

--- vllm/engine/arg_utils.py
+++ vllm/engine/arg_utils.py
@@ -359,14 +359,6 @@ class EngineArgs:
                 self.tokenizer_pool_type,
                 self.tokenizer_pool_extra_config,
             ), self.ray_workers_use_nsight)
-                                   model_config.get_sliding_window(),
-                                   self.enable_prefix_caching)
-        parallel_config = ParallelConfig(self.pipeline_parallel_size,
-                                         self.tensor_parallel_size,
-                                         self.worker_use_ray,
-                                         self.max_parallel_loading_workers,
-                                         self.disable_custom_all_reduce,
-                                         self.ray_workers_use_nsight)
         scheduler_config = SchedulerConfig(self.max_num_batched_tokens,
                                            self.max_num_seqs,
                                            model_config.max_model_len,
