b11e11d924ac1c156ec9b7a197fc8670367ae3c3
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-03-22 18:05:46 +0000

    move response decoding to becnhmark_serving

--- neuralmagic/benchmarks/scripts/common.py
+++ neuralmagic/benchmarks/scripts/common.py
@@ -12,8 +12,7 @@ from vllm import (LLM, SamplingParams, __version__ as __vllm_version__)
 from vllm.outputs import RequestOutput
 from vllm.transformers_utils.tokenizer import get_tokenizer
 from .datasets_registry import SHAREGPT_PATH, SHAREGPT_DOWNLOAD_STR
-from .backend_request_func import (RequestFuncInput, RequestFuncOutput,
-                                   async_request_vllm)
+from .backend_request_func import (RequestFuncInput, RequestFuncOutput, AsyncRequestVLLM)
 from ...tools.call_cmd import call_cmd
 
 
@@ -169,7 +168,7 @@ def warmup_server(server_host: int,
             )
             tasks.append(
                 asyncio.create_task(
-                    async_request_vllm(request_func_input=request_func_input)))
+                    AsyncRequestVLLM.async_request_vllm(request_func_input=request_func_input)))
         _ = await asyncio.gather(*tasks)
 
     tokenizer = get_tokenizer(model)
