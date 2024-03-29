175b4b13d9682d72e514efe180f9053d605d4b0f
Dipika Sikka <dipikasikka1@gmail.com>
2024-03-29 18:55:47 +0000

    add counter for each function

--- vllm/timings/timer.py
+++ vllm/timings/timer.py
@@ -25,7 +25,7 @@ class Timer:
         self.enable_logging = enable_logging
         self.measurements = {}
         self.avg_after_iterations = avg_after_iterations
-        self.current_iter = 0
+        self.current_iter = {}
         self.lock = RLock()
 
     @contextmanager
@@ -45,35 +45,37 @@ class Timer:
             end = time.time()
             with self.lock:
                 self._update(func_name, end - start)
-                self._compute_average()
+                self._compute_average(func_name)
 
     def _update(self, func_name: str, time_elapased: float):
         """
-        Update the dictionary of measurements. 
+        Update the dictionary of measurements and counter for each function call
 
         :param func_name: name of the function that will be used to log the 
             measurements
         :param time_elapsed: time taken for function execution  
         """
-        self.current_iter += 1
+        if self.current_iter.get(func_name) is None:
+            self.current_iter[func_name] = 1
+        else:
+            self.current_iter[func_name] += 1
+
         if self.measurements.get(func_name) is None:
             self.measurements[func_name] = [time_elapased]
         else:
             self.measurements[func_name].append(time_elapased)
 
-    def _compute_average(self):
+    def _compute_average(self, func_name: str):
         """
         Determine the number of iterations. Display averages if this value
         is equivalent to self.avg_after_iterations.
 
-        TODO: Do we want to keep track of each function individually and only 
-        display the average when a specific function reaches a certain number 
-        of iterations or one counter for all timer calls? Do we want to clear
-        the values after the average is calculated?
+        :param func_name: name of the function that will be used to log the 
+            measurements
+
+        TODO: Do we want to clear the values after the average is calculated?
         """
-        if self.current_iter == self.avg_after_iterations:
-            for func_name in self.measurements:
-                _LOGGER.info(f"Average time for {func_name}: ")
-                _LOGGER.info(
-                    str(numpy.average(self.measurements.get(func_name))))
-            self.current_iter = 0
+        if self.current_iter.get(func_name) == self.avg_after_iterations:
+            _LOGGER.info(f"Average time for {func_name}: ")
+            _LOGGER.info(str(numpy.average(self.measurements.get(func_name))))
+            self.current_iter[func_name] = 0
