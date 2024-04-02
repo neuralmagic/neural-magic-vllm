c97fde6786508f9fb86ea127e63c5a0e09db5853
dbogunowicz <damian@neuralmagic.com>
2024-04-02 10:14:46 +0000

    ruff

--- tests/model_executor/test_weight_utils.py
+++ tests/model_executor/test_weight_utils.py
@@ -6,6 +6,7 @@ from transformers import AutoModelForCausalLM, AutoConfig
 from sparseml.transformers import SparseAutoModelForCausalLM
 import shutil
 
+
 @pytest.fixture
 def hf_folder_dense(tmp_path):
     model_id = "stas/tiny-random-llama-2"
@@ -13,37 +14,41 @@ def hf_folder_dense(tmp_path):
     yield snapshot_download(model_id, cache_dir=cache_dir)
     shutil.rmtree(cache_dir)
 
+
 @pytest.fixture
 def hf_folder_sparse(tmp_path):
     model_id = "mgoin/TinyLlama-1.1B-Chat-v1.0-pruned2.4-compressed"
     cache_dir = tmp_path / "cache"
     yield snapshot_download(model_id, cache_dir=cache_dir)
     shutil.rmtree(cache_dir)
-     
 
 
 def test_hf_model_weights_iterator_dense_compressor(hf_folder_dense):
     with init_empty_weights():
         config = AutoConfig.from_pretrained(hf_folder_dense)
-        model = AutoModelForCausalLM.from_config(config) 
+        model = AutoModelForCausalLM.from_config(config)
     assert not hasattr(config, "sparsity_config")
     model_weight_names = set([name for name, _ in model.named_parameters()])
-    loaded_weight_names = set(name for name, _ in hf_model_weights_iterator(hf_folder_dense))
+    loaded_weight_names = set(
+        name for name, _ in hf_model_weights_iterator(hf_folder_dense))
     assert model_weight_names == loaded_weight_names
-    
-def test_hf_model_weights_iterator_sparse_compressor(tmp_path, hf_folder_sparse):
+
+
+def test_hf_model_weights_iterator_sparse_compressor(tmp_path,
+                                                     hf_folder_sparse):
     # save the sparse model using the compressed safetensor format
     model = SparseAutoModelForCausalLM.from_pretrained(hf_folder_sparse)
     save_path = tmp_path / "save_path"
-    model.save_pretrained(save_path, save_compressed=True, skip_compression_stats=True)
-    
+    model.save_pretrained(save_path,
+                          save_compressed=True,
+                          skip_compression_stats=True)
+
     # test the weight loading logic
     with init_empty_weights():
         config = AutoConfig.from_pretrained(save_path)
-        model = AutoModelForCausalLM.from_config(config) 
+        model = AutoModelForCausalLM.from_config(config)
     assert hasattr(config, "sparsity_config")
     model_weight_names = set([name for name, _ in model.named_parameters()])
-    loaded_weight_names = set(name for name, _ in hf_model_weights_iterator(save_path))
+    loaded_weight_names = set(
+        name for name, _ in hf_model_weights_iterator(save_path))
     assert model_weight_names == loaded_weight_names
-    
-    
