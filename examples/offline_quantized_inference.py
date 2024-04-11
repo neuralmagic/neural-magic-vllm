c2c59a9135dfdf322e3866dc0be818b2d213b31e
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-04-11 18:19:30 +0000

    use hf-model

--- examples/offline_quantized_inference.py
+++ examples/offline_quantized_inference.py
@@ -1,8 +1,8 @@
 from vllm import LLM, SamplingParams
 import torch
 
-model_path="/home/varun/code/vllm/llama-models/Nous-Hermes-Llama2-13b/quantized_model/llama-13b/Nous-Hermes-Llama2-13b-smoothquant/"
-tokenizer="/home/varun/code/vllm/llama-models/Nous-Hermes-Llama2-13b/"
+hf_path="nm-testing/Nous-Hermes-Llama2-13b-smoothquant"
+model_path=hf_path
 
 # Sample prompts.
 prompts = [
@@ -13,13 +13,11 @@ prompts = [
 ]
 
 # Create a sampling params object.
-#sampling_params = SamplingParams(temperature=0.0, top_p=0.95)
 sampling_params = SamplingParams(temperature=0.0, top_k = 1,max_tokens=20)
 
 # Create an LLM.
 llm = LLM(
     model=model_path,
-    tokenizer=tokenizer,
     gpu_memory_utilization=0.9,
     max_model_len=2048,
     quantization="smoothquant",
@@ -27,14 +25,6 @@ llm = LLM(
     enforce_eager=True,
     tensor_parallel_size=1,
     max_num_batched_tokens=7000)
-
-#llm = LLM(
-#    model=tokenizer,
-#    tokenizer=tokenizer,
-#    gpu_memory_utilization=0.9,
-#    max_model_len=2048,
-#    max_num_batched_tokens=7000)
-
 
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
