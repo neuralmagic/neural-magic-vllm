fbbca782c8a619ab82404611508a0f5b18abec51
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-04-22 20:20:26 +0000

    Use only cutlass gemm and dq

--- vllm/model_executor/layers/quantization/smoothquant/config.py
+++ vllm/model_executor/layers/quantization/smoothquant/config.py
@@ -126,24 +126,26 @@ class SmoothQuantLinearMethod(LinearMethodBase):
             name.replace(suffix, "dequant_scale")
         return name
 
-    def scales_shard_splitter(self, 
-                              param: torch.Tensor,
-                              loaded_weight: torch.Tensor, 
-                              shard_id: Union[str, int]) -> Tuple[torch.Tensor, torch.Tensor]:
-        """Index into param for for loading.
-        
-        This function is called by QKVColumnLinear and MergedColumnParallelLinear
-        during weight loading to put the scales from disk in the right spot.
-        """
-        if type(shard_id) == str:
-            qkv_idxs = { "q": 0, "k": 1, "v": 2 }
-            if shard_id not in qkv_idxs:
-                raise ValueError(f"Invalid shard_id {shard_id}")
-            shard_id = qkv_idxs[shard_id]
-        elif type(shard_id) != int:
-            raise ValueError(f"Invalid shard id {shard_id}")
+    def shard_id_as_int(self, shard_id: Union[str, int]) -> int:
+        if isinstance(shard_id, int):
+            return shard_id
 
-        return param[shard_id], loaded_weight
+        assert isinstance(shard_id, str)
+        qkv_idxs = { "q": 0, "k": 1, "v": 2 }
+        assert shard_id in qkv_idxs
+        return qkv_idxs[shard_id]
+
+    def scales_shard_splitter(self,
+                              param: torch.Tensor,
+                              loaded_weight: torch.Tensor,
+                              shard_id: Union[str, int],
+                              logical_widths: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        shard_id = self.shard_id_as_int(shard_id)
+        offset = sum(logical_widths[:shard_id]) 
+        size = logical_widths[shard_id]
+        # update loaded weight with copies for broadcast.
+        loaded_weight = loaded_weight.repeat(size)
+        return param[offset : offset + size], loaded_weight
 
     def get_layer_format(self, layer_name: str) -> SmoothQuantFormat:
         """
@@ -216,16 +218,22 @@ class SmoothQuantLinearMethod(LinearMethodBase):
             "output_dim": 0,
         })
 
-        # Static scale for each logical weight (e.g. 3 for QKV).
-        dequant_scale = Parameter(
-            torch.empty(
-                len(output_sizes_per_partition), 
-                 device='cuda', dtype=params_dtype,
-            ), requires_grad=False
-        )
-        set_weight_attrs(dequant_scale, {
-            "shard_splitter": self.scales_shard_splitter,
-        })
+        if len(output_sizes_per_partition) == 1:
+            # Single static scale for the entire tensor.
+            dequant_scale = Parameter(
+                torch.empty((1),device='cuda', dtype=params_dtype),
+                requires_grad=False
+            )
+        else:
+            # Static scale for each logical weight (e.g. 3 for QKV).
+            dequant_scale = Parameter(
+                torch.empty((sum(output_sizes_per_partition)),
+                             device='cuda', dtype=params_dtype),
+                requires_grad=False
+            )
+            set_weight_attrs(dequant_scale,
+                             {"shard_splitter": self.scales_shard_splitter,
+                              "logical_widths" : output_sizes_per_partition})
 
         return {
             "weight": weight,
@@ -266,6 +274,8 @@ class SmoothQuantLinearMethod(LinearMethodBase):
             dtype: Datatype to dequantize to.
         Returns:
             x_dq: dequantized activation at output_dtype precision
+
+        NOTE: This function is unused, but could be handy when debugging.
         """
         # Split X_q and X_dq buffer into logical activations (for QKV case).
         x_q_split = x_q.split(logical_widths, dim=-1)
@@ -292,47 +302,13 @@ class SmoothQuantLinearMethod(LinearMethodBase):
             raise NotImplementedError
         weight_q = weights["weight"]
         static_scales = weights["dequant_scale"]
-        logical_widths = weights["logical_widths"]
         sq_format = weights["sq_format"]
 
         # Q
         x_q, activation_scales = self._quantize(x, sq_format)
 
-        if isinstance(sq_format, SmoothQuantStaticPerTensor):
-            # TODO (varun) : Move scale_to_broadcast to create_weights
-            scale_to_broadcast = torch.zeros((sum(logical_widths)))
-            filled_idx = 0 
-            for scale, size in zip(static_scales, logical_widths):
-                indices = torch.arange(filled_idx, filled_idx + size)
-                scale_to_broadcast.index_fill_(0, indices, scale)
-                filled_idx += size
-
-            return cutlass_gemm_dq(x_q, weight_q, x.dtype,
-                                   scale_to_broadcast)
-        else:
-            ## GEMM
-            #x_q = x_q.view(-1, x_q.shape[-1])
-            #a_q = torch.empty((x_q.shape[0], weight_q.shape[0]), dtype=torch.int32, device="cuda")
-            #print (f"a_q {a_q.shape} {a_q.dtype} - activation_scales {activation_scales.shape} {activation_scales.dtype} \n")
-            #self.i8cugemm.linear_a8_w8_o32_(x_q, weight_q, a_q)
-            #a_q = a_q.view(*x_q.shape[:-1], -1)
-            ## DQ
-            #dq = self._dequantize(a_q, activation_scales, static_scales,
-            #                      logical_widths, x.dtype, sq_format)
-
-            # TODO (varun) : Move scale_to_broadcast to create_weights
-            scale_to_broadcast = torch.zeros((sum(logical_widths)))
-            filled_idx = 0 
-            for scale, size in zip(static_scales, logical_widths):
-                indices = torch.arange(filled_idx, filled_idx + size)
-                scale_to_broadcast.index_fill_(0, indices, scale)
-                filled_idx += size
-
+        # GEMM and DQ
+        if activation_scales is not None:
             activation_scales = activation_scales[:, None]
-
-            cutlass_dq = cutlass_gemm_dq(x_q, weight_q, x.dtype, scale_to_broadcast,
-                                              activation_scales)
-            
-            #torch.allclose(dq, cutlass_dq)
-            return cutlass_dq
+        return cutlass_gemm_dq(x_q, weight_q, x.dtype, static_scales, activation_scales)
 
