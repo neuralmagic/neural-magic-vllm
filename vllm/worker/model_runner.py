5092c94440daaebc083c955df595d019a9e20031
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-04-29 18:22:02 +0000

    reset modelrunner

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -1071,10 +1071,9 @@ class ModelRunner:
         return self.model_config.get_vocab_size()
 
 
-class CUDAGraphRunner(nn.Module):
+class CUDAGraphRunner():
 
     def __init__(self, model: nn.Module):
-        super().__init__()
         self.model = model
         self.input_buffers: Dict[str, torch.Tensor] = {}
         self.output_buffers: Dict[str, torch.Tensor] = {}
