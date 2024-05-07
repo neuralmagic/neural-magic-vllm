f34cae89b70242085984bc97a100c12b818dc08e
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-05-07 17:42:42 +0000

    refactor reduction utils

--- csrc/quantization/compressed_tensors/int8_quant_kernels.cu
+++ csrc/quantization/compressed_tensors/int8_quant_kernels.cu
@@ -3,6 +3,7 @@
 #include <assert.h>
 
 #include "../../dispatch_utils.h"
+#include "../../reduction_utils.cuh"
 
 static inline __device__ int8_t float_to_int8_rn(float x)
 {
@@ -18,33 +19,6 @@ static inline __device__ int8_t float_to_int8_rn(float x)
 }
 
 namespace vllm {
-
-// TODO (varun) : Merge this into reduction utils and use the existing interface
-// TODO (varun) : Add unit tests for this
-template<typename T>
-__inline__ __device__ T warpReduceMax(T val)
-{
-#pragma unroll
-    for (int mask = 16; mask > 0; mask >>= 1)
-        val = max(val, __shfl_xor_sync(0xffffffff, val, mask, 32));
-    return val;
-}
-
-/* Calculate the maximum of all elements in a block */
-template<typename T>
-__inline__ __device__ T blockReduceMax(T val)
-{
-    static __shared__ T shared[32];
-    int                 lane = threadIdx.x & 0x1f;  // in-warp idx
-    int                 wid  = threadIdx.x >> 5;    // warp idx
-    val = warpReduceMax(val);  // get maxx in each warp
-    if (lane == 0)  // record in-warp maxx by warp Idx
-        shared[wid] = val;
-    __syncthreads();
-    val = (threadIdx.x < (blockDim.x / 32.f)) ? shared[lane] : -1e20f;
-    val = warpReduceMax(val);
-    return val;
-}
 
 template <typename scalar_t, typename scale_type>
 __global__ void static_scaled_int8_quant_kernel(
