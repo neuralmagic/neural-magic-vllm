8b8f8fe1c7f8a4e7ccc0623574c608a01645dd2d
Domenic Barbuzzi <domenic@neuralmagic.com>
2024-05-23 16:07:27 +0000

    Remove clean_shutdown test skip

--- tests/engine/test_multiproc_workers.py
+++ tests/engine/test_multiproc_workers.py
@@ -1,6 +1,4 @@
 import asyncio
-# UPSTREAM SYNC
-import sys
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 from time import sleep
@@ -99,10 +97,6 @@ def test_local_workers() -> None:
         assert isinstance(e, ChildProcessError)
 
 
-# UPSTREAM SYNC
-@pytest.mark.skipif(sys.version_info < (3, 10),
-                    reason="This test is inexplicably failing in CI "
-                    "on Python < 3.10")
 def test_local_workers_clean_shutdown() -> None:
     """Test clean shutdown"""
 
