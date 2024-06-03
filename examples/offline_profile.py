682eae50bdb8f903af16c232a66759c6814a2c95
Varun Sundar Rabindranath <varun@neuralmagic.com>
2024-06-03 17:30:26 +0000

    fix interface change

--- examples/offline_profile.py
+++ examples/offline_profile.py
@@ -82,14 +82,18 @@ def run_profile(context: ProfileContext, csv_output: Optional[str],
         sys.exit(-1)
 
     for i in range(batch_size):
+        prompt_token_ids=torch.randint(
+            128,  # 128 to skip over special tokens
+            llm.llm_engine.model_config.get_vocab_size() // 2,
+            size=(prompt_len, )).tolist()
+
+        from vllm.inputs import TokensPrompt
+        prompt_token_ids = {'prompt_token_ids' : prompt_token_ids}
+
         llm.llm_engine.add_request(
             request_id=f"seq{i}",
-            prompt=None,
-            prompt_token_ids=torch.randint(
-                128,  # 128 to skip over special tokens
-                llm.llm_engine.model_config.get_vocab_size() // 2,
-                size=(prompt_len, )).tolist(),
-            sampling_params=sampling_params)
+            inputs = prompt_token_ids,
+            params=sampling_params)
 
     with nm_profile() as prefill_prof:
         llm.llm_engine.step()  # First step is prefill
