ec71544411efffe2021420742150771105a184a6
Robert Shaw <rshaw@neuralmagic.com>
2024-06-09 13:08:40 +0000

    fix falcon

--- tests/conftest.py
+++ tests/conftest.py
@@ -564,12 +564,13 @@ class VllmRunner:
         block_size: int = 16,
         enable_chunked_prefill: bool = False,
         swap_space=4,
+        trust_remote_code: bool = True,
         **kwargs,
     ) -> None:
         self.model = LLM(
             model=model_name,
             tokenizer=tokenizer_name,
-            trust_remote_code=True,
+            trust_remote_code=trust_remote_code,
             dtype=dtype,
             swap_space=swap_space,
             disable_log_stats=disable_log_stats,
@@ -694,28 +695,6 @@ def vllm_runner():
 
 # UPSTREAM SYNC: needed for nm-automation
 class VllmRunnerNm(VllmRunner):
-
-    def __init__(
-        self,
-        model_name: str,
-        sparsity: Optional[str] = None,
-        tokenizer_name: Optional[str] = None,
-        dtype: str = "half",
-        disable_log_stats: bool = True,
-        tensor_parallel_size: int = 1,
-        max_model_len: Optional[int] = None,
-    ) -> None:
-        self.model = LLM(
-            model=model_name,
-            sparsity=sparsity,
-            tokenizer=tokenizer_name,
-            trust_remote_code=True,
-            dtype=dtype,
-            swap_space=0,
-            disable_log_stats=disable_log_stats,
-            tensor_parallel_size=tensor_parallel_size,
-            max_model_len=max_model_len,
-        )
 
     def generate_w_logprobs(
         self,
