0393d45986e899a926dd9418d590c88d37f44bbc
Cyrus Leung <tlleungac@connect.ust.hk>
2024-06-15 12:45:31 +0800

    [mypy] Enable type checking for test directory (#5017)

--- tests/distributed/test_utils.py
+++ tests/distributed/test_utils.py
@@ -12,7 +12,7 @@ if should_skip_test_group(group_name="TEST_DISTRIBUTED"):
 
 
 @ray.remote
-class _CUDADeviceCountStatelessTestActor():
+class _CUDADeviceCountStatelessTestActor:
 
     def get_count(self):
         return cuda_device_count_stateless()
@@ -28,7 +28,8 @@ def test_cuda_device_count_stateless():
     """Test that cuda_device_count_stateless changes return value if
     CUDA_VISIBLE_DEVICES is changed."""
 
-    actor = _CUDADeviceCountStatelessTestActor.options(num_gpus=2).remote()
+    actor = _CUDADeviceCountStatelessTestActor.options(  # type: ignore
+        num_gpus=2).remote()
     assert sorted(ray.get(
         actor.get_cuda_visible_devices.remote()).split(",")) == ["0", "1"]
     assert ray.get(actor.get_count.remote()) == 2
