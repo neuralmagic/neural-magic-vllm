9ace1dc9daf3871e2f0faad286f808db7ff0b4ce
Robert Shaw <rshaw@neuralmagic.com>
2024-06-19 21:39:25 +0000

    fixed

--- tests/distributed/test_same_node.py
+++ tests/distributed/test_same_node.py
@@ -1,14 +1,11 @@
-import os
+# UPSTREAM SYNC:
+# Since this test is launched with torchrun, pytest.skip
+# an importing from test directory is having trouble.
+# So, we can have the should_skip_test_group logic here.
 
-import pytest
 import torch
 
-from tests.nm_utils.utils_skip import should_skip_test_group
 from vllm.distributed.parallel_state import is_in_the_same_node
-
-if should_skip_test_group(group_name="TEST_DISTRIBUTED"):
-    pytest.skip("TEST_DISTRIBUTED=DISABLE, skipping distributed test group",
-                allow_module_level=True)
 
 torch.distributed.init_process_group(backend="gloo")
 test_result = is_in_the_same_node(torch.distributed.group.WORLD)
