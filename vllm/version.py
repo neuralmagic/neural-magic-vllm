8485c2e3245f35de10546fda69ed603e8a245df3
Tyler Michael Smith <tyler@neuralmagic.com>
2024-07-02 15:10:30 +0000

    format

--- vllm/version.py
+++ vllm/version.py
@@ -6,8 +6,8 @@ try:
     __commit__ = vllm.commit_id.__commit__
 except Exception as e:
     warnings.warn(f"failed to read commit hash:\n{e}",
-                      RuntimeWarning,
-                      stacklevel=2)
+                  RuntimeWarning,
+                  stacklevel=2)
     __commit__ = "COMMIT_HASH_PLACEHOLDER"
 
 __version__ = "0.5.1"
