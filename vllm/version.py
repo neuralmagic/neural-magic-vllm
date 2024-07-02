6d7c6cb10c1a09b45d227b9ed1aae0ec29948b84
Tyler Michael Smith <tyler@neuralmagic.com>
2024-07-02 15:01:43 +0000

    Generate commit ID in an untracked file

--- vllm/version.py
+++ vllm/version.py
@@ -1,3 +1,8 @@
 # UPSTREAM SYNC: take downstream
+try:
+    import vllm.commit_id
+    __commit__ = vllm.commit_id.__commit__
+except:
+    __commit__ = "COMMIT_HASH_PLACEHOLDER"
+    
 __version__ = "0.5.1"
-__commit__ = "COMMIT_HASH_PLACEHOLDER"
