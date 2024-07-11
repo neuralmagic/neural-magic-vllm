d0979d3d2ac70ed865bc3fd939b2e46617adc321
dhuangnm <dhuang@MacBook-Pro-2.local>
2024-07-11 13:14:19 -0400

    gc first

--- tests/accuracy/test_lm_eval_correctness.py
+++ tests/accuracy/test_lm_eval_correctness.py
@@ -118,10 +118,10 @@ def test_lm_eval_correctness(num_gpus_available):
 
         # clean up memory
         destroy_model_parallel()
+        gc.collect()
         torch.cuda.empty_cache()
         torch.distributed.destroy_process_group()
         ray.shutdown()
-        gc.collect()
 
         # Make sure the server finishes tearing down.
         time.sleep(10.)
