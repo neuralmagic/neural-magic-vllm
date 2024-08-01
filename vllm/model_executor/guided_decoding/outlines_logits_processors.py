73722a2343adc25b01e535fabbc0717b6a297c7a
rshaw@neuralmagic.com <rshaw@neuralmagic.com>
2024-08-01 16:06:56 +0000

    nit

--- vllm/model_executor/guided_decoding/outlines_logits_processors.py
+++ vllm/model_executor/guided_decoding/outlines_logits_processors.py
@@ -39,7 +39,7 @@ class BaseLogitsProcessor:
                  scores: torch.Tensor) -> torch.Tensor:
         """Use the FSM to bias the logits before sampling the next token."""
         seq_id = hash(tuple(input_ids))
-    
+
         if len(input_ids) > 0:
             last_token = input_ids[-1]
             last_seq_id = hash(tuple(input_ids[:-1]))
